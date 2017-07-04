96743b45bdffef5d2358d02c7c6b1cc4b30d9bc4
Carlos Martinez <martinezcj2@gmail.com>
2017-07-04 14:28:23 -0400

    Add scene to multiple projects

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -2,20 +2,26 @@ $(document).ready(function(){
     // Init Scrollmagic
     const controller  = new ScrollMagic.Controller();
 
-    // Build a scene
-    const ourScene = new ScrollMagic.Scene({
-        triggerElement: '#project01'
-    }) 
+    //  loop through each project element
+    $('.project').each(function(){     
+        // Build a scene
+        const ourScene = new ScrollMagic.Scene({
+            triggerElement: this.children[0],
+            duration: '80%', //just # is pixels, with percentage is % of the vh
+            triggerHook: 0.7,
+            reverse: false // animation only happens once
+        }) 
 
-    .setClassToggle('#project01', 'fade-in') //add class to project01
-    .addIndicators({
-        name: 'fade-in',
-        colorTrigger:'black',
-        indent: 200,
-        colorStart: '#75C659'
-    }) // requires plugin to fdo this
-    .addTo(controller);
-
+        .setClassToggle(this, 'fade-in') //add class to project01
+        .addIndicators({
+            name: 'fade-in',
+            colorTrigger:'black',
+            indent: 100,
+            colorStart: '#75C695',
+            colorEnd: 'pink'
+        }) // requires plugin to fdo this
+        .addTo(controller);
+    })
 });
 
 
