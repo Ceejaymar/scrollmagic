11cec56065170cc5034f4f68ed5a07a164710469
Carlos Martinez <martinezcj2@gmail.com>
2017-07-04 01:36:51 -0400

    Initial commit

---
+++ 01-intro/start/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    // Init Scrollmagic
+    const controller  = new ScrollMagic.Controller();
+
+    // Build a scene
+    const ourScene = new ScrollMagic.Scene({
+
+    }) 
+
+    .setClassToggle('#project01', 'fade-in') //add class to project01
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
