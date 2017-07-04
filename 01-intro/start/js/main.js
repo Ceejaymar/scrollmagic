b9dd724e0473c111b808ad532aa5530e8b02c8e6
Carlos Martinez <martinezcj2@gmail.com>
2017-07-04 02:15:22 -0400

    Create scene for project01

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -4,10 +4,17 @@ $(document).ready(function(){
 
     // Build a scene
     const ourScene = new ScrollMagic.Scene({
-
+        triggerElement: '#project01'
     }) 
 
     .setClassToggle('#project01', 'fade-in') //add class to project01
+    .addIndicators({
+        name: 'fade-in',
+        colorTrigger:'black',
+        indent: 200,
+        colorStart: '#75C659'
+    }) // requires plugin to fdo this
+    .addTo(controller);
 
 });
 
